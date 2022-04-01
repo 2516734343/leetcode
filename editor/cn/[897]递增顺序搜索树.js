ed19736c1e9dce8f5612aba9d9c84a3f4b6cf7da
2516734343 <2516734343@qq.com>
2022-04-01 22:40:55 +0800

    feat: 二叉树

---
+++ editor/cn/[897]递增顺序搜索树.js
@@ -0,0 +1,59 @@
+//给你一棵二叉搜索树的 root ，请你 按中序遍历 将其重新排列为一棵递增顺序搜索树，使树中最左边的节点成为树的根节点，并且每个节点没有左子节点，只有一个右
+//子节点。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
+//输出：[1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+//
+//
+// 示例 2：
+//
+//
+//输入：root = [5,1,7]
+//输出：[1,null,5,null,7]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点数的取值范围是 [1, 100]
+// 0 <= Node.val <= 1000
+//
+// Related Topics 栈 树 深度优先搜索 二叉搜索树 二叉树
+// 👍 269 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var increasingBST = function(root) {
+    if (root == null) return null;
+    const left = increasingBST(root.left);
+    const right = increasingBST(root.right);
+    root.left = null;
+    root.right = right;
+    if (left == null) return root;
+    let p = left;
+    while (p != null && p.right != null) {
+        p = p.right;
+    }
+    p.right = root;
+    return left;
+};
+//leetcode submit region end(Prohibit modification and deletion)
